f8a4ee6bf077ebf28e0dd9fe960caab5ec08641f
andycat <andy@infiniterecursion.com.au>
2010-07-26 01:04:51 -0700

    mencoder fix , script tweak

--- run_indytube.sh
+++ run_indytube.sh
@@ -2,6 +2,7 @@
 
 #kick off indytube
 #you should remember to check you have all the right permissions set.
+cd /opt/indytube/indytube-prod/
 
 python2.5 indytube.py >run_indytube.log 2>&1 &
 echo $! > indytube.pid
