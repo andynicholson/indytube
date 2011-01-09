06ee5862b2dcaf2bb000945fec5b16e854c8fffc
andycat <andy@infiniterecursion.com.au>
2011-01-08 23:53:19 -0800

    remove encoder lock file

--- run_indytube.sh
+++ run_indytube.sh
@@ -4,5 +4,6 @@
 #you should remember to check you have all the right permissions set.
 cd /opt/indytube/indytube-prod/
 
+rm encoder.lock.0
 python2.5 indytube.py >run_indytube.log 2>&1 &
 echo $! > indytube.pid
