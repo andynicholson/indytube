0d547e7c38a6331bb0537d4ab99fe274943eca7e
andycat <andy@infiniterecursion.com.au>
2011-03-30 23:11:13 -0700

    make sure we kill common subprocesses when restarting

--- restart_indytube.sh
+++ restart_indytube.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 INDYTUBEPID=`cat indytube.pid`
+
+#kill possible subprocesses
+killall qt-faststart
+killall ffmpeg
+killall mencoder
+
 kill $INDYTUBEPID
 sleep 2
 kill -9 $INDYTUBEPID
 rm indytube-encoder.lock.*
 rm wetube.log
 rm run_indytube.log
-./run_indytube.sh
+./run_indytube.sh 
